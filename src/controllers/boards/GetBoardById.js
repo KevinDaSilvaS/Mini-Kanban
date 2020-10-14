e5346dd67c2db1c6a87ff81f768a68620572ca0b
Your Name <kevin.kds80@gmail.com>
2020-10-14 20:08:28 -0300

    Added get board by id feature

--- src/controllers/boards/GetBoardById.js
+++ src/controllers/boards/GetBoardById.js
@@ -1,5 +1,18 @@
+const response = require('../../app/response');
+const Status = require('../../constants/HttpCodes');
+const {Boards} = require('../../operations');
+
 const execute = async (req, res) => {
-    res.send({});
+    try {
+        const {boardId} = req.params;
+        const board = await Boards.get({_id: boardId});
+
+        if(!board) response(res, Status.NOT_FOUND, "Board not found.");
+
+        response(res, Status.OK, board);
+    } catch (error) {
+        response(res, Status.INTERNAL_SERVER_ERROR, error);
+    }
 }
 
 module.exports = execute;
