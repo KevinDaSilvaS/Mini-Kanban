022e21d6f75bed973c3da2bf81a26816ba0107e3
Your Name <kevin.kds80@gmail.com>
2020-10-14 20:19:16 -0300

    Added update board feature

--- src/controllers/boards/UpdateBoard.js
+++ src/controllers/boards/UpdateBoard.js
@@ -1,5 +1,19 @@
+const response = require('../../app/response');
+const Status = require('../../constants/HttpCodes');
+const {Boards} = require('../../operations');
+
 const execute = async (req, res) => {
-    res.send({});
+    try {
+        const {boardId} = req.params;
+        if(await Boards.update({_id: boardId} ,req.body)) {
+            return response(res, Status.OK, {});
+        }
+
+        response(res, Status.BAD_REQUEST, "Unable to update user.");
+
+    } catch (error) {
+        response(res, Status.INTERNAL_SERVER_ERROR, error);
+    }
 }
 
 module.exports = execute;
