eecdbb1d9c88c48df22508008eebd8023b11407a
Your Name <kevin.kds80@gmail.com>
2020-10-15 20:32:31 -0300

    Added Delete task feature

--- src/controllers/tasks/DeleteTask.js
+++ src/controllers/tasks/DeleteTask.js
@@ -1,5 +1,18 @@
+const response = require('../../app/response');
+const Status = require('../../constants/HttpCodes');
+const {TASK_NOT_FOUND} = require('../../constants/ErrorMessages');
+const {Tasks} = require('../../operations');
+
 const execute = async (req, res) => {
-    res.send({});
+    try {
+        const {taskId} = req.params;
+        const {n} = await Tasks.delete({_id: taskId});
+        if(!n || n <= 0) response(res, Status.NOT_FOUND, TASK_NOT_FOUND);
+
+        response(res, Status.NO_CONTENT, undefined);
+    } catch (error) {
+        response(res, Status.INTERNAL_SERVER_ERROR, error);
+    }
 }
 
 module.exports = execute;
