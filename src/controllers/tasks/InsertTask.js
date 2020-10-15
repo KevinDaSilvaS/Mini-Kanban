6d7cdaa1e363c6139fa3d9b4cbe175cc36eeacbb
Your Name <kevin.kds80@gmail.com>
2020-10-14 21:00:48 -0300

    Started insert task Feature

--- src/controllers/tasks/InsertTask.js
+++ src/controllers/tasks/InsertTask.js
@@ -1,8 +1,25 @@
+const axios = require('axios');
 const response = require('../../app/response');
 const Status = require('../../constants/HttpCodes');
+const {Tasks} = require('../../operations');
 
 const execute = async (req, res) => {
-    response(res, Status.CREATED, {oi: "oi"});
+    
+
+    try {
+        const paramsBoardId = req.params.boardId;
+
+        const data = await axios.get(`http://localhost:1747/boards/${paramsBoardId}`);
+
+        const {_id, title, description, boardId, status} = await Tasks.insert({
+            ...req.body, boardId: paramsBoardId});
+
+        const insertedTask = {taskId: _id, title, description, boardId, status};
+        response(res, Status.CREATED, insertedTask);
+
+    } catch (error) {
+        response(res, Status.INTERNAL_SERVER_ERROR, error);
+    }
 }
 
 module.exports = execute;
