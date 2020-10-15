69629aebdef884a891be9e4d5b442fa0d0d90e35
Your Name <kevin.kds80@gmail.com>
2020-10-15 19:51:19 -0300

    Created get task by id feature

--- src/controllers/tasks/GetTaskById.js
+++ src/controllers/tasks/GetTaskById.js
@@ -1,5 +1,20 @@
+const response = require('../../app/response');
+const Status = require('../../constants/HttpCodes');
+const {TASK_NOT_FOUND} = require('../../constants/ErrorMessages');
+const {Tasks} = require('../../operations');
+
 const execute = async (req, res) => {
-    res.send({params: req.params});
+    try {
+        const {boardId, taskId} = req.params;
+        const {task, description, status} = await Tasks.get({
+            _id: taskId, boardId});
+
+        if(!task) response(res, Status.NOT_FOUND, TASK_NOT_FOUND);
+
+        response(res, Status.OK, {task, description, status});
+    } catch (error) {
+        response(res, Status.INTERNAL_SERVER_ERROR, error);
+    }
 }
 
 module.exports = execute;
