0dca4f7928be2fe84ecab969c33769adb8a68370
Your Name <kevin.kds80@gmail.com>
2020-10-15 20:29:41 -0300

    Added Update task feature

--- src/controllers/tasks/UpdateTask.js
+++ src/controllers/tasks/UpdateTask.js
@@ -1,5 +1,20 @@
+const response = require('../../app/response');
+const Status = require('../../constants/HttpCodes');
+const {ERROR_UPDATE_TASK} = require('../../constants/ErrorMessages');
+const {Tasks} = require('../../operations');
+
 const execute = async (req, res) => {
-    res.send({});
+    try {
+        const {taskId} = req.params;
+        if(await Tasks.update({_id: taskId} ,req.body)) {
+            return response(res, Status.NO_CONTENT, undefined);
+        }
+
+        response(res, Status.BAD_REQUEST, ERROR_UPDATE_TASK);
+
+    } catch (error) {
+        response(res, Status.INTERNAL_SERVER_ERROR, error);
+    }
 }
 
 module.exports = execute;
