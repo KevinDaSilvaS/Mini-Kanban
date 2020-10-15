e8a2b20ea63af81d9571b19182c94d122cfcf08d
Your Name <kevin.kds80@gmail.com>
2020-10-15 19:46:32 -0300

    Created get all tasks feature

--- src/controllers/tasks/GetTasks.js
+++ src/controllers/tasks/GetTasks.js
@@ -1,5 +1,21 @@
+const response = require('../../app/response');
+const Status = require('../../constants/HttpCodes');
+const {Tasks} = require('../../operations');
+
 const execute = async (req, res) => {
-    res.send({});
+    try {
+        const {boardId} = req.params;
+        let tasks = await Tasks.getAll({boardId});
+        tasks = tasks.map((task) => {
+            const {_id, title, description, status} = task;
+            return {taskId: _id, title, description, status};
+        });
+
+        response(res, Status.OK, tasks);
+        
+    } catch (error) {
+        response(res, Status.INTERNAL_SERVER_ERROR, error);
+    }
 }
 
 module.exports = execute;
