e8a97692d524814314137d8789249ea5c36fd83b
Your Name <kevin.kds80@gmail.com>
2020-10-15 19:58:46 -0300

    Created Get Task By Id feature

--- src/controllers/tasks/GetTaskById.js
+++ src/controllers/tasks/GetTaskById.js
@@ -6,12 +6,13 @@ const {Tasks} = require('../../operations');
 const execute = async (req, res) => {
     try {
         const {boardId, taskId} = req.params;
-        const {task, description, status} = await Tasks.get({
+
+        const {title, description, status} = await Tasks.get({
             _id: taskId, boardId});
 
-        if(!task) response(res, Status.NOT_FOUND, TASK_NOT_FOUND);
+        if(!title) return response(res, Status.NOT_FOUND, TASK_NOT_FOUND);
 
-        response(res, Status.OK, {task, description, status});
+        response(res, Status.OK, {title, description, status});
     } catch (error) {
         response(res, Status.INTERNAL_SERVER_ERROR, error);
     }
