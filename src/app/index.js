c954738e6728e9f24b0d697cb53cbb03f45af6c3
Your Name <kevin.kds80@gmail.com>
2020-10-18 15:20:03 -0300

    Added cors to project

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,10 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
