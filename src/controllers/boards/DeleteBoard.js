690a539ef65ebeec81f411a90651b38804e234ed
Your Name <kevin.kds80@gmail.com>
2020-10-20 19:54:57 -0300

    Added remove all tasks mof a board in delete board feature

--- src/controllers/boards/DeleteBoard.js
+++ src/controllers/boards/DeleteBoard.js
@@ -1,11 +1,13 @@
 const response = require('../../app/response');
 const Status = require('../../constants/HttpCodes');
 const {BOARD_NOT_FOUND} = require('../../constants/ErrorMessages');
-const {Boards} = require('../../operations');
+const {Boards, Tasks} = require('../../operations');
 
 const execute = async (req, res) => {
     try {
         const {boardId} = req.params;
+        await Tasks.delete({boardId});
+
         const {n} = await Boards.delete({_id: boardId});
         if(!n || n <= 0) response(res, Status.NOT_FOUND, BOARD_NOT_FOUND);
 
