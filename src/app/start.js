53b2745361edaf3d1ee35bff3074990628aa365f
Your Name <kevin.kds80@gmail.com>
2021-03-05 19:33:44 -0300

    Removed hardcoded port and auth data to database

--- src/app/start.js
+++ src/app/start.js
@@ -1,8 +1,9 @@
+require('dotenv/config');
 const connectionMongo = require('../database/mongo/connection/connection');
 connectionMongo.then().catch(e => new Error(e));
 
 const app = require('./index');
-const port = 1747;
+const port = process.env.PORT || 1747;
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- src/database/mongo/connection/connection.js
+++ src/database/mongo/connection/connection.js
@@ -1,8 +1,11 @@
+require('dotenv/config');
 const mongoose = require('mongoose');
+
+const {MONGO_USER, MONGO_PASSWORD} = process.env;
 
 const connection = async () => {
     //in local mode change mongo to localhost
-    return await mongoose.connect('mongodb://kevindasilva:123@mongo:27017/admin', 
+    return await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@mongo:27017/admin`, 
     {useNewUrlParser: true, useUnifiedTopology: true},
     (err) => {
         if (err) {
