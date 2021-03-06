74790fc8ccc14702a821ba796aada836c0912c02
Your Name <kevin.kds80@gmail.com>
2021-03-06 09:48:25 -0300

    Add one more .env alias to file

--- src/database/mongo/connection/connection.js
+++ src/database/mongo/connection/connection.js
@@ -1,11 +1,11 @@
 require('dotenv/config');
 const mongoose = require('mongoose');
 
-const {MONGO_USER, MONGO_PASSWORD} = process.env;
+const {MONGO_USER, MONGO_PASSWORD, MONGO_INITDB_DATABASE} = process.env;
 
 const connection = async () => {
     //in local mode change mongo to localhost
-    return await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@mongo:27017/admin`, 
+    return await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@mongo:27017/${MONGO_INITDB_DATABASE}`, 
     {useNewUrlParser: true, useUnifiedTopology: true},
     (err) => {
         if (err) {
