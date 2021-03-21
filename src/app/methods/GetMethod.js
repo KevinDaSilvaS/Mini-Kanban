647818cb272af53227d2918057d8b5b8d1101453
Your Name <kevin.kds80@gmail.com>
2021-03-21 19:05:40 -0300

    Add for loop to make all validations work

--- src/app/methods/GetMethod.js
+++ src/app/methods/GetMethod.js
@@ -5,8 +5,8 @@ class GetMethod extends HttpMethodBase {
     getRoute(app, route) {
         return app.get(route.route, async (req, res) => {
             const values = Object.values(route.validations);
-            values.map = async (validation) => {
-                await validation(req, res);
+            for (let i = 0; i < values.length; i++) {
+                await values[i](req, res);
             }
 
             await route.controller(req, res);
