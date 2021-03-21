bf29702517c79c33f81560677b286a49e96b2be0
Your Name <kevin.kds80@gmail.com>
2021-03-21 20:58:20 -0300

    Add verification to guarantee controllers will not be called if validations fail

--- src/app/methods/GetMethod.js
+++ src/app/methods/GetMethod.js
@@ -6,7 +6,9 @@ class GetMethod extends HttpMethodBase {
         return app.get(route.route, async (req, res) => {
             const values = Object.values(route.validations);
             for (let i = 0; i < values.length; i++) {
-                await values[i](req, res);
+                const result = await values[i](req, res);
+                if(!result) 
+                    return;
             }
 
             await route.controller(req, res);
