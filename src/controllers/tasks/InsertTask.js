5fad91759db05f9a22113183bcf5bb9f844f5dda
Your Name <kevin.kds80@gmail.com>
2021-03-22 19:38:33 -0300

    Add check to guarantee board exist before inserting task

--- src/controllers/tasks/InsertTask.js
+++ src/controllers/tasks/InsertTask.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const response = require('../../app/response');
 const Status = require('../../constants/HttpCodes');
+const {TASK_NOT_FOUND} = require('../../constants/ErrorMessages');
 const {Tasks} = require('../../operations');
 
 const execute = async (req, res) => {
@@ -8,7 +9,13 @@ const execute = async (req, res) => {
     try {
         const paramsBoardId = req.params.boardId;
 
-        await axios.get(`http://localhost:1747/boards/${paramsBoardId}`);
+        const board = await axios.get(`
+        http://localhost:1747/boards/${paramsBoardId}`);
+
+        if(board.statusCode != 200) {
+            response(res, Status.NOT_FOUND, TASK_NOT_FOUND);
+            return;
+        }
 
         const {_id, title, description, boardId, status} = await Tasks.insert({
             ...req.body, boardId: paramsBoardId});
