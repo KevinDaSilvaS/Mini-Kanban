0ce272c64479423a9b4f698256b948cdc3fadf6c
Your Name <kevin.kds80@gmail.com>
2021-03-23 21:13:00 -0300

    Fix throw of error 500 instead of 400

--- src/controllers/boards/UpdateBoard.js
+++ src/controllers/boards/UpdateBoard.js
@@ -7,13 +7,17 @@ const execute = async (req, res) => {
     try {
         const { boardId } = req.params;
 
-        if(await Boards.update({ _id: boardId } ,req.body)) {
-            return response(res, Status.NO_CONTENT, undefined);
-        }
+        const board = await Boards.update({ _id: boardId } ,req.body);
 
-        response(res, Status.BAD_REQUEST, ERROR_UPDATE_BOARD);
+        if(board.ok > 0)
+            return response(res, Status.NO_CONTENT, undefined);
+
+        throw ERROR_UPDATE_BOARD;
 
     } catch (error) {
+        if(error == ERROR_UPDATE_BOARD)
+            return response(res, Status.BAD_REQUEST, ERROR_UPDATE_BOARD);
+
         response(res, Status.INTERNAL_SERVER_ERROR, error);
     }
 }
--- src/controllers/tasks/UpdateTask.js
+++ src/controllers/tasks/UpdateTask.js
@@ -6,13 +6,17 @@ const {Tasks} = require('../../operations');
 const execute = async (req, res) => {
     try {
         const { taskId } = req.params;
-        if(await Tasks.update({ _id: taskId } ,req.body)) {
-            return response(res, Status.NO_CONTENT, undefined);
-        }
 
-        response(res, Status.BAD_REQUEST, ERROR_UPDATE_TASK);
+        const task = await Tasks.update({ _id: taskId } ,req.body);
+        if(task.ok > 0)
+            return response(res, Status.NO_CONTENT, undefined);
+
+        throw ERROR_UPDATE_TASK;
 
     } catch (error) {
+        if(error == ERROR_UPDATE_TASK)
+            return response(res, Status.BAD_REQUEST, ERROR_UPDATE_TASK);
+
         response(res, Status.INTERNAL_SERVER_ERROR, error);
     }
 }
