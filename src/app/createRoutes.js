6250c128504b1730f0fbf93b2692b5bcbc56e816
Your Name <kevin.kds80@gmail.com>
2021-03-27 18:40:49 -0300

    Turn create routes process into an object instead of a switch case structure

--- src/app/createRoutes.js
+++ src/app/createRoutes.js
@@ -1,28 +1,7 @@
 const app = require('./index');
 const routes = require('../routes/routes');
-const GetMethod = require('./methods/GetMethod');
-const PostMethod = require('./methods/PostMethod');
-const PatchMethod = require('./methods/PatchMethod');
-const DeleteMethod = require('./methods/DeleteMethod');
+const HttpMethods = require('./methods/');
 
 routes.map((route) => {
-    switch (route.method) {
-        case 'GET':
-            new GetMethod().getRoute(app, route);
-            break;
-        
-        case 'POST':
-            new PostMethod().getRoute(app, route);
-            break;
-
-        case 'PATCH':
-            new PatchMethod().getRoute(app, route);
-            break;
-
-        case 'DELETE':
-            new DeleteMethod().getRoute(app, route);
-            break;
-    }
+    HttpMethods[route.method](app, route);
 });
-
-
