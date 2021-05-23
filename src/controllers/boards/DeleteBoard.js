c259c3ab2ce61541ee7caf2d71b0c506390aee28
Your Name <kevin.kds80@gmail.com>
2021-05-23 14:19:44 -0300

    Add message publishing to delete controllers

--- src/controllers/boards/DeleteBoard.js
+++ src/controllers/boards/DeleteBoard.js
@@ -1,5 +1,13 @@
 const execute = async (req, res, dependencies) => {
-    const { response, Status, ErrorMessages, Operations } = dependencies;
+    const { 
+        response, 
+        Status, 
+        ErrorMessages, 
+        Operations,
+        AmqpBroker,
+        BrokerInfo,
+        channel
+    } = dependencies;
     const { Boards, Tasks } = Operations;
     const { BOARD_NOT_FOUND } = ErrorMessages;
 
@@ -11,6 +19,14 @@ const execute = async (req, res, dependencies) => {
         if(!n || n <= 0) {
             return response(res, Status.NOT_FOUND, BOARD_NOT_FOUND);
         }
+
+        const {exchanges, queues} = BrokerInfo;
+        await AmqpBroker.publish(
+            channel, 
+            exchanges.topic_mini_kanban.exchange,
+            queues.delete_all_comments_when_task_or_board_is_deleted,
+            JSON.stringify({ boardId })
+        );
 
         return response(res, Status.NO_CONTENT, undefined);
     } catch (error) {
--- src/controllers/tasks/DeleteTask.js
+++ src/controllers/tasks/DeleteTask.js
@@ -1,6 +1,14 @@
 
 const execute = async (req, res, dependencies) => {
-    const { response, Status, ErrorMessages, Operations } = dependencies;
+    const { 
+        response, 
+        Status, 
+        ErrorMessages, 
+        Operations,
+        AmqpBroker,
+        BrokerInfo,
+        channel 
+    } = dependencies;
     const { Tasks } = Operations;
     const { TASK_NOT_FOUND } = ErrorMessages;
     
@@ -11,6 +19,14 @@ const execute = async (req, res, dependencies) => {
         if(!n || n <= 0) {
             return response(res, Status.NOT_FOUND, TASK_NOT_FOUND);
         }
+
+        const {exchanges, queues} = BrokerInfo;
+        await AmqpBroker.publish(
+            channel, 
+            exchanges.topic_mini_kanban.exchange,
+            queues.delete_all_comments_when_task_or_board_is_deleted,
+            JSON.stringify({ taskId })
+        );
 
         return response(res, Status.NO_CONTENT, undefined);
     } catch (error) {
